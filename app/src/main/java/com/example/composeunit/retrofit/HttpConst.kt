7524ca20ab35842305ce31347ded0970a11f889f
wangfei44 <wangfei44@lenovo.com>
2023-04-06 19:11:40 +0800

    修改key

--- app/src/main/java/com/example/composeunit/retrofit/HttpConst.kt
+++ app/src/main/java/com/example/composeunit/retrofit/HttpConst.kt
@@ -5,7 +5,7 @@ package com.example.composeunit.retrofit
  */
 class HttpConst {
     companion object {
-        private const val CHAT_GTP_KEY = "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"
+        private const val CHAT_GTP_KEY = "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"
         const val CHAT_AUTHORIZATION = "Bearer $CHAT_GTP_KEY"
         const val CHAT_GTP_CONTENT_TYPE = "application/json"
         const val CHAT_GTP_ROLE = "user"
