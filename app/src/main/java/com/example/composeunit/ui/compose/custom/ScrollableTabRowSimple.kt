dce9e70323b6dbe00dbcde2e4b4c96671de4a16b
luhenchang <1276998208@qq.com>
2023-10-10 18:04:10 +0800

    提交代码：1、安全验证，用户信息的获取。2、和参数personId和personRegionalismCode获取

--- app/src/main/java/com/example/composeunit/ui/compose/custom/ScrollableTabRowSimple.kt
+++ app/src/main/java/com/example/composeunit/ui/compose/custom/ScrollableTabRowSimple.kt
@@ -2,6 +2,12 @@ package com.example.composeunit.ui.compose.custom
 
 import android.util.Log
 import androidx.annotation.FloatRange
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -22,32 +28,47 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Typography
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -55,9 +76,172 @@ import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
+import com.example.composeunit.R
 import com.example.composeunit.ui.theme.openAiLight
 import kotlinx.coroutines.launch
 import kotlin.math.abs
+
+val customFontFamily = FontFamily(
+    Font(R.font.rubik_bold)
+)
+
+val typography = Typography(
+    defaultFontFamily = customFontFamily,
+    h1 = TextStyle(
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
+    ),
+    // 添加其他文本样式
+)
+
+@Preview
+@Composable
+fun CanvasDemon(wCount: Int = 20, hCount: Int = 100) {
+    Canvas(Modifier.fillMaxSize().background(Color.White)) {
+        drawIntoCanvas {
+            val outPath = Path()
+            val scale = 60f
+            val paint = Paint().apply {
+                color = Color.Red.copy(alpha = 0.5f)
+                strokeWidth = 2f
+                style = PaintingStyle.Stroke
+            }
+            val hPath = Path().apply {
+                moveTo(0f, 0f)
+                lineTo(size.width, 0f)
+            }
+            it.save()
+            for (i in 0..hCount) {
+                it.drawPath(path = hPath, paint = paint)
+                it.translate(0f, scale)
+            }
+            it.restore()
+            val wPath = Path().apply {
+                moveTo(0f, 0f)
+                lineTo(0f, size.height)
+            }
+            it.save()
+            for (i in 0..wCount) {
+                it.drawPath(path = wPath, paint = paint)
+                it.translate(scale, 0f)
+            }
+            it.restore()
+
+            it.save()
+            for (outIndex in 0..wCount) {
+                it.nativeCanvas.translate(scale*outIndex,0f)
+                it.save()
+                for (i in 0..hCount) {
+                    it.translate(0f, scale)
+                    it.nativeCanvas.drawText(
+                        i.toString(),
+                        0,
+                        i.toString().length,
+                        scale / 2f,
+                        scale / 2f,
+                        paint.asFrameworkPaint()
+                    )
+                }
+                it.restore()
+            }
+            it.restore()
+
+            val centerH = scale/2
+            outPath.moveTo(centerH*3,centerH*3)
+            outPath.lineTo(centerH*7,centerH*5)
+            outPath.lineTo(centerH*1,centerH*7)
+            outPath.lineTo(centerH*13,centerH*9)
+            outPath.lineTo(centerH*1,centerH*11)
+            outPath.lineTo(centerH*21,centerH*13)
+            outPath.lineTo(centerH*3,centerH*15)
+            outPath.lineTo(centerH*21,centerH*17)
+            outPath.lineTo(centerH*13,centerH*19)
+            outPath.lineTo(centerH*21,centerH*21)
+            outPath.lineTo(centerH*21,centerH*23)
+            outPath.lineTo(centerH*7,centerH*25)
+            outPath.lineTo(centerH*21,centerH*27)
+            outPath.lineTo(centerH*1,centerH*29)
+
+
+            it.drawPath(path = outPath, paint = paint.apply { color = Color.Green })
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TextDemo() {
+    val scaleAnim by remember { mutableStateOf(Animatable(initialValue = 1f)) }
+    LaunchedEffect(Unit) {
+        scaleAnim.animateTo(
+            targetValue = 1.1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(1000, easing = LinearEasing), repeatMode = RepeatMode.Reverse
+            )
+        )
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "adb汉字123",
+            style = TextStyle(
+                fontSize = (33).sp,
+                color = Color.Black
+            ),
+            modifier = Modifier.scale(scaleAnim.value)
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun ScrollDemo() {
+    LazyRow(
+        Modifier
+            .wrapContentWidth()
+            .fillMaxHeight()
+            .background(Color.Red)
+            .padding(50.dp)
+    ) {
+        item {
+            LazyRow(
+                Modifier
+                    .width(200.dp)
+                    .fillMaxHeight()
+                    .background(Color.Green),
+            ) {
+                items(100) {
+                    Text("index = $it", fontFamily = FontFamily.Monospace)
+                }
+            }
+        }
+        item {
+            Box(
+                Modifier
+                    .size(500.dp)
+                    .background(Color.Yellow)
+            )
+        }
+
+        item {
+            LazyRow(
+                Modifier
+                    .width(200.dp)
+                    .fillMaxHeight()
+                    .background(Color.Black),
+            ) {
+                items(100) {
+                    Text("index = $it")
+                }
+            }
+        }
+    }
+}
 
 @Preview
 @OptIn(ExperimentalFoundationApi::class)
@@ -107,44 +291,47 @@ fun ScrollableTabRowSimple() {
         HorizontalPager(
             state = pagerState,
             modifier = Modifier
-                .fillMaxHeight(),
-            pageNestedScrollConnection = object : NestedScrollConnection {
-                fun Velocity.consumeOffsetOnOrientations(orientation: Orientation): Velocity {
-                    return if (orientation == Orientation.Vertical) {
-                        copy(x = 0f)
-                    } else {
-                        copy(y = 0f)
-                    }
-                }
-
-                fun Offset.consumeVelocityOnOrientations(orientation: Orientation): Offset {
-                    return if (orientation == Orientation.Vertical) {
-                        copy(x = 0f)
-                    } else {
-                        copy(y = 0f)
-                    }
-                }
-
-                override fun onPostScroll(
-                    consumed: Offset,
-                    available: Offset,
-                    source: NestedScrollSource
-                ): Offset {
-                    return when (source) {
-                        NestedScrollSource.Fling -> available.consumeVelocityOnOrientations(Orientation.Horizontal)
-                        else -> Offset.Zero
-                    }
-                }
-
-                override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
-                    return available.consumeOffsetOnOrientations(Orientation.Horizontal)
-                }
-            }
+                .fillMaxHeight()
+//            pageNestedScrollConnection = object : NestedScrollConnection {
+//                fun Velocity.consumeOffsetOnOrientations(orientation: Orientation): Velocity {
+//                    return if (orientation == Orientation.Vertical) {
+//                        copy(x = 0f)
+//                    } else {
+//                        copy(y = 0f)
+//                    }
+//                }
+//
+//                fun Offset.consumeVelocityOnOrientations(orientation: Orientation): Offset {
+//                    return if (orientation == Orientation.Vertical) {
+//                        copy(x = 0f)
+//                    } else {
+//                        copy(y = 0f)
+//                    }
+//                }
+//
+//
+//                override fun onPostScroll(
+//                    consumed: Offset,
+//                    available: Offset,
+//                    source: NestedScrollSource
+//                ): Offset {
+//                    Log.e("onPostScroll consumed: Velocity x=",consumed.x.toString())
+//                    Log.e("onPostScroll available: Velocity x=",available.x.toString())
+//                    return when (source) {
+//                        NestedScrollSource.Fling -> available.consumeVelocityOnOrientations(Orientation.Horizontal)
+//                        else -> Offset.Zero
+//                    }
+//                }
+//
+//                override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+//                    Log.e("consumed: Velocity x=",consumed.x.toString())
+//                    Log.e("available: Velocity x=",available.x.toString())
+//                    return available.consumeOffsetOnOrientations(Orientation.Horizontal)
+//                }
+//            }
         ) { pagePosition ->
             SecondPager(pagerState)
         }
-
-
     }
 
 
@@ -158,8 +345,8 @@ private fun SecondPager(stateOne: PagerState) {
     }
     val pagerState = rememberPagerState(pageCount = { data.size })
     val draggablePagerState = pagerState.canScrollBackward
-    Log.e("pagerState===",draggablePagerState.toString())
-    Log.e("pagerState===",pagerState.currentPageOffsetFraction.toString())
+    Log.e("pagerState===", draggablePagerState.toString())
+    Log.e("pagerState===", pagerState.currentPageOffsetFraction.toString())
     val scope = rememberCoroutineScope()
     Column {
         ScrollableTabRow(
@@ -202,46 +389,43 @@ private fun SecondPager(stateOne: PagerState) {
                 }
             }
         }
-//        val draggableState = rememberDraggablePagerState(stateOne, pagerState)
-//        draggableState.initUserScrollEnableType()
+        val draggableState = rememberDraggablePagerState(stateOne, pagerState)
+        draggableState.initUserScrollEnableType()
 
         //Log.e("scrollState=", scrollState.value.toString())
         HorizontalPager(
-           // userScrollEnabled = draggableState.userScrollEnabled(),
+            //userScrollEnabled = draggableState.userScrollEnabled(),
             state = pagerState,
             modifier = Modifier
                 .fillMaxHeight()
-                .pointerInput(Unit){
-                   detectHorizontalDragGestures { change, dragAmount ->
-                       Log.e("detectHorizontalDragGestures=", change.toString())
-                       Log.e("detectHorizontalDragGestures dragAmount=", dragAmount.toString())
-                   }
+                .pointerInput(Unit) {
+                    detectHorizontalDragGestures { change, dragAmount ->
+                        Log.e("detectHorizontalDragGestures=", change.toString())
+                        Log.e("detectHorizontalDragGestures dragAmount=", dragAmount.toString())
+                    }
                 }
                 .draggable(
                     state = rememberDraggableState { onDetail ->
                         scope.launch {
+                            Log.e("onPostScroll1 consumed: Velocity x=", onDetail.toString())
                             //draggableState.setDraggableOnDetailToScrollToPage(onDetail)
                         }
                     },
                     orientation = Orientation.Horizontal,
+                    enabled = true
+                    //enabled = (draggableState.draggableEnabled())
+                )
+                .draggable(
+                    state = rememberDraggableState { onDetail ->
+                        scope.launch {
+                            Log.e("onPostScroll2 consumed: Velocity x=", onDetail.toString())
+                            //draggableState.setDraggableOnDetailToScrollToPage(onDetail)
+                        }
+                    },
+                    orientation = Orientation.Horizontal,
+                    enabled = true
                     //enabled = (draggableState.draggableEnabled())
                 ),
-            pageNestedScrollConnection = object : NestedScrollConnection {
-                override fun onPostScroll(
-                    consumed: Offset,
-                    available: Offset,
-                    source: NestedScrollSource
-                ): Offset {
-                    Log.e("tag two", "consumed=$consumed")
-                    return when (source) {
-                        else -> Offset.Zero
-                    }
-                }
-
-                override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
-                    return consumed
-                }
-            }
         ) { pagePosition ->
             Log.e("tag", "加载页码$pagePosition")
             Box(
@@ -323,7 +507,10 @@ fun PagerTabIndicator(
 
             val canvasHeight = size.height
             val padding =
-                minOf(paddingIndicatorWidth.toPx(), indicatorWidth + indicatorWidth * abs(fraction))
+                minOf(
+                    paddingIndicatorWidth.toPx(),
+                    indicatorWidth + indicatorWidth * abs(fraction)
+                )
             drawRoundRect(
                 color = color,
                 topLeft = Offset(
